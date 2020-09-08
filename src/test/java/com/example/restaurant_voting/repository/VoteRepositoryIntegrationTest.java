594387726c238c5f80c94137f7c30a1a104af96d
ivankov_aleksej <ivankov.aleksej@gmail.com>
2020-09-08 19:26:43 +0300

    integration test repository with to

--- src/test/java/com/example/restaurant_voting/repository/VoteRepositoryIntegrationTest.java
+++ src/test/java/com/example/restaurant_voting/repository/VoteRepositoryIntegrationTest.java
@@ -1,11 +1,13 @@
 package com.example.restaurant_voting.repository;
 
 import com.example.restaurant_voting.model.Vote;
+import com.example.restaurant_voting.to.VoteTo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -42,5 +44,30 @@ class VoteRepositoryIntegrationTest {
         assertEquals(1, votes.size());
         assertEquals(DATE, votes.get(0).getDate());
         assertEquals(1, votes.get(0).getUser().getId());
+    }
+
+    @Test
+    void getStatisticByDate() {
+        Page<VoteTo> voteTos = voteRepository.getStatisticByDate(DATE, PageRequest.of(0, 20));
+        assertEquals(2, voteTos.getContent().size());
+        assertEquals(1, voteTos.getContent().get(0).getCount());
+        assertEquals(2, voteTos.getContent().get(1).getCount());
+    }
+
+    @Test
+    void getStatisticAll() {
+        Page<VoteTo> voteTos = voteRepository.getStatisticAll(PageRequest.of(0, 20,
+                Sort.by(new Sort.Order(Sort.Direction.DESC, "date"), new Sort.Order(Sort.Direction.ASC, "menu.id"))));
+        assertEquals(4, voteTos.getContent().size());
+        assertEquals(2, voteTos.getContent().get(0).getCount());
+        assertEquals(1, voteTos.getContent().get(1).getCount());
+        assertEquals(1, voteTos.getContent().get(2).getCount());
+        assertEquals(2, voteTos.getContent().get(3).getCount());
+
+        assertEquals(3, voteTos.getContent().get(0).getMenu().getId());
+        assertEquals(4, voteTos.getContent().get(1).getMenu().getId());
+        assertEquals(1, voteTos.getContent().get(2).getMenu().getId());
+        assertEquals(2, voteTos.getContent().get(3).getMenu().getId());
+
     }
 }
